c1c06128a7ea45fee474eefce7a7eebcd14b03c6
prameshbajra <pe.messh@gmail.com>
2020-06-29 16:30:07 +0530

    Clean up till here ...
    WOrking for key in react native

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const goalInputHandler = (enteredGoal) => {
+        setEnteredGoal(enteredGoal);
+    }
+
+    const addGoalHandler = () => {
+        setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    }
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Enter something ..." style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
+                <Button title="Add" onPress={addGoalHandler} />
+            </View>
+            <View>
+                {
+                    courseGoals.map((goal) => {
+                        return (
+                            <Text>{goal}</Text>
+                        )
+                    })
+                }
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 40
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        borderColor: 'black',
+        borderWidth: 2,
+        padding: 3,
+        width: '80%'
+    }
 });
